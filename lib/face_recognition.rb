76d65f5b3986b0844bc7e5d3063212d6caae3845
Isaac Norman <isaacdnorman@gmail.com>
2015-01-14 17:22:23 +1100

    Update to include then engine etc. in the base file

--- lib/face_recognition.rb
+++ lib/face_recognition.rb
@@ -1,2 +1,20 @@
+require "face_recognition/engine"
+
 module FaceRecognition
+end
+
+# load Rails/Railtie
+begin
+  require 'rails'
+rescue LoadError
+  #do nothing
+end
+
+# Load All the stuff we need
+require 'face_recognition/models/base'
+require 'face_recognition/models/omni_auth'
+require 'face_recognition/models/anonymous'
+
+if defined? Rails
+  require 'face_recognition/railtie'
 end
